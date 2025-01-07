f71224f0d225134821db0dc237132873611c6c3b
Orphey98 <stalker664@gmail.com>
2025-01-07 19:02:16 +0200

    - Replacement by type logic added - Fixed bug with "remove-expired-item: false" logic - Improved user messages order - Added debug messages for item replacement

--- src/main/java/dev/anhcraft/timedmmoitems/task/CheckTask.java
+++ src/main/java/dev/anhcraft/timedmmoitems/task/CheckTask.java
@@ -6,10 +6,8 @@ import static dev.anhcraft.timedmmoitems.TimedMMOItems.EXPIRY_PERIOD;
 import dev.anhcraft.timedmmoitems.TimedMMOItems;
 import dev.anhcraft.timedmmoitems.config.ItemConfig;
 import io.lumine.mythic.lib.api.item.NBTItem;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import net.Indyuce.mmoitems.api.Type;
 import net.Indyuce.mmoitems.api.item.mmoitem.LiveMMOItem;
 import net.Indyuce.mmoitems.stat.data.DoubleData;
 import org.bukkit.ChatColor;
@@ -20,6 +18,7 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class CheckTask extends BukkitRunnable {
   private final TimedMMOItems plugin;
+  private boolean itemsDroppedFlag;
 
   public CheckTask(TimedMMOItems plugin) {
     this.plugin = plugin;
@@ -32,12 +31,18 @@ public class CheckTask extends BukkitRunnable {
         continue;
       }
 
+      itemsDroppedFlag = false;
+
       boolean needUpdate = false;
       int rmvCounter = 0;
 
-      boolean needReplace = false;
-      int rplCounter = 0;
-      HashMap<String, Integer> rplMap = new HashMap<>();
+      boolean needItemReplace = false;
+      int rplItemsCounter = 0;
+      HashMap<String, Integer> rplItemsMap = new HashMap<>();
+
+      boolean needTypeReplace = false;
+      int rplTypesCounter = 0;
+      HashMap<String, Integer> rplTypesMap = new HashMap<>();
 
       List<ItemStack> newItems = new LinkedList<>();
 
@@ -86,11 +91,25 @@ public class CheckTask extends BukkitRunnable {
             && ((DoubleData) mmo.getData(EXPIRY_DATE)).getValue() < System.currentTimeMillis()) {
           rmvCounter += item.getAmount();
           needUpdate = true;
-          if (plugin.config.expiredItemReplace.containsKey(mmo.getId())) {
-            rplCounter += item.getAmount();
-            rplMap.put(mmo.getId(), rplCounter);
-            needReplace = true;
+        }
+        if (!plugin.config.expiredTypeReplace.isEmpty()) {
+          Type type = mmo.getType();
+          String typeName =
+              type.toString()
+                  .substring(type.toString().indexOf("id='") + 4, type.toString().lastIndexOf("'"));
+          if (plugin.config.expiredTypeReplace.containsKey(typeName)) {
+            rplTypesCounter += item.getAmount();
+            rplTypesMap.put(typeName, rplTypesCounter);
+            needTypeReplace = true;
+            needUpdate = true;
           }
+        }
+        if (!plugin.config.expiredItemReplace.isEmpty()
+            && plugin.config.expiredItemReplace.containsKey(mmo.getId())) {
+          rplItemsCounter += item.getAmount();
+          rplItemsMap.put(mmo.getId(), rplItemsCounter);
+          needItemReplace = true;
+          needUpdate = true;
         }
       }
 
@@ -100,21 +119,30 @@ public class CheckTask extends BukkitRunnable {
       if (plugin.config.forceUpdateInventory) player.updateInventory();
 
       if (rmvCounter > 0) {
-        if (needReplace) itemReplace(player, rplMap);
         String msg =
             plugin.config.expiredItemRemoved.replace("%amount%", Integer.toString(rmvCounter));
         player.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
         player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 1.0f, 1.0f);
       }
+      if (needItemReplace) itemReplace(player, rplItemsMap, false);
+      if (needTypeReplace) itemReplace(player, rplTypesMap, true);
+      if (itemsDroppedFlag) {
+        String msg = plugin.config.replacedItemDropped;
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+      }
     }
   }
 
-  private void itemReplace(Player player, HashMap<String, Integer> map) {
+  private void itemReplace(Player player, HashMap<String, Integer> map, Boolean replaceByType) {
     for (Map.Entry<String, Integer> entry : map.entrySet()) {
       String itemId = entry.getKey();
       int rplCounter = entry.getValue();
-      List<ItemConfig> itemConfigList = plugin.config.expiredItemReplace.get(itemId);
+      List<ItemConfig> itemConfigList;
 
+      itemConfigList =
+          Boolean.TRUE.equals(replaceByType)
+              ? plugin.config.expiredTypeReplace.get(itemId)
+              : plugin.config.expiredItemReplace.get(itemId);
       for (ItemConfig itemConfig : itemConfigList) {
         try {
           ItemStack itemStack = ReplaceFactory.createItemStack(itemConfig, rplCounter);
@@ -125,9 +153,17 @@ public class CheckTask extends BukkitRunnable {
             for (ItemStack leftover : leftovers.values()) {
               player.getWorld().dropItem(player.getLocation(), leftover);
             }
-            String msg = plugin.config.replacedItemDropped;
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+            itemsDroppedFlag = true;
           }
+          TimedMMOItems.plugin.debug(
+              3,
+              "%s item (%s) has been replaced by (%s). Replacement by type: %b. Leftovers on the"
+                  + " ground: %b",
+              player.getName(),
+              itemId,
+              itemConfig,
+              replaceByType,
+              !leftovers.isEmpty());
         } catch (IllegalArgumentException e) {
           plugin.getLogger().severe(e.getMessage());
         }
